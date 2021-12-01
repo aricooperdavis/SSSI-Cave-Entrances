7934b495fe1a9cef9c665e0fcd73a18f47753223
Ari Cooper-Davis <cooper-davis.arran@cooper-davis.net>
2021-12-01 12:53:04 +0000

    Push to github

---
+++ README.md
@@ -0,0 +1,22 @@
+# SSSI Cave Entrances
+This project illustrates how many of cave and mine entrances (and other karst features such as sinks, resurgences, and digs) can be found on Special Sites of Scientific Interest in Great Britain.
+
+> **Note**: the results of this analysis are only as reliable as the source data used. Whilst some work has been done to correct obvious mistakes in the source data, it cannot be assumed to be completely accurate.
+
+## Data sources
+### Entrances/features
+Feature data for points of interest is collected from a variety of public databases:
+- [Cambrian Caving Council | Registry](https://www.cambriancavingcouncil.org.uk/registry/ccr_registry.php)
+- [Council of Northern Caving Clubs | Caves List](https://cncc.org.uk/caving/caves/index.php?keyword=&sort=last_updated%20DESC)
+- [Derbyshire Caving Association | Peak District Caving](https://peakdistrictcaving.info/home/the-caves/search)
+- [Devon & Cornwall Underground Council | Registry](https://dcuc.org.uk/registry/r/registrysearch.php)
+- [Forest of Dean Cave Conservation & Access Group | Registry](https://www.fodccag.org.uk/registry/registrysearch.php)
+- [Grampian Speleological Group | Registry](https://registry.gsg.org.uk/sr/registrysearch.php)
+- [Mendip Cave Registry and Archive | Registry](https://www.mcra.org.uk/registry/registrysearch.php)
+- [Northern Caves | Caves Index](https://northerncaves.co.uk/caves/)
+
+### SSSIs
+SSSIs databases are maintain by each government and are publicly available:
+- England: [Natural England](https://naturalengland-defra.opendata.arcgis.com/datasets/Defra::sites-of-special-scientific-interest-england/about)
+- Scotland: [Scottish Natural Heritage](https://gateway.snh.gov.uk/natural-spaces/dataset.jsp?dsid=SSSI)
+- Wales: [Natural Resources Wales](https://lle.gov.wales/catalogue/item/ProtectedSitesSitesOfSpecialScientificInterest/?lang=en)
---
+++ map/leaflet_map.js
@@ -0,0 +1,107 @@
+let map = L.map('map').setView([56, -3.5], 6);
+
+// Background mapping tile layer
+//L.tileLayer('http://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+    subdomains: ['a','b','c'],
+    maxZoom: 18,
+    //id: 'mapbox/streets-v11',
+    //tileSize: 512,
+    //zoomOffset: -1,
+    //accessToken: 'pk.eyJ1IjoiYXJpY29vcGVyZGF2aXMiLCJhIjoiY2t3bHo1NjN4MXI3aTJ3bWQ4cXA4b2ljciJ9.bV0roZ5YiqR6PhMmzIQZcw'
+}).addTo(map);
+
+// SSSI WMS layers
+let england = L.tileLayer.wms('http://environment.data.gov.uk/spatialdata/sites-of-special-scientific-interest-england/wms', {
+    layers: 'Sites_of_Special_Scientific_Interest_England',
+    format: 'image/png',
+    transparent: 'true',
+    minZoom: 11,
+    opacity: 0.5,
+    //minNativeZoom: 11,
+});
+england.addTo(map);
+let scotland = L.tileLayer.wms('http://cagmap.snh.gov.uk/arcgis/services/snh_protected_sites/MapServer/WMSServer', {
+    layers: '2',
+    format: 'image/png',
+    transparent: 'true',
+    minZoom: 11,
+    //minNativeZoom: 1,
+});
+scotland.addTo(map);
+let wales = L.tileLayer.wms('http://lle.gov.wales/services/wms/nrw', {
+    layers: 'NRW_SSSI',
+    format: 'image/png',
+    transparent: 'true',
+    minZoom: 11,
+    //minNativeZoom: 1,
+});
+wales.addTo(map);
+
+// Feature markers
+let registries = {
+    'CCC': '#ff7f0e',
+    'CNCC': '#1f77b4',
+    'DCA': '#2ca02c',
+    'DCUC': '#e377c2',
+    'FoDCCAG': '#9467bd',
+    'GSG': '#8c564b',
+    'MCRA': '#d62728',
+    'Northern Caves': '#7f7f7f'
+};
+let caves = {}
+fetch('features.geojson')
+    .then(response => response.json())
+    .then(jsonResponse => {
+
+        // Add geoJSON layers to object and add to map
+        Object.keys(registries).forEach(function(source) {
+            caves[source] = L.geoJSON(jsonResponse, {
+                filter: function(feature, layer) {
+                    return feature.properties.source_registry == source;
+                },
+                pointToLayer: function(feature, latlng) {
+                    return L.circleMarker(latlng, {
+                        radius: 5,
+                        color: registries[feature.properties.source_registry],
+                        opacity: 1,
+                        fillOpacity: 0.5,
+                    });
+                },
+                onEachFeature: function(feature, layer) {
+                    layer.bindPopup(`<b>${feature.properties.cave_name}</b><br><b>SSSI</b>: ${feature.properties.sssi}<br><b>Source</b>: ${feature.properties.source_registry}`);
+                },
+            });
+        });
+        Object.keys(caves).forEach(source => caves[source].addTo(map));
+
+        // Control UI
+        caves = Object.fromEntries(Object.entries(caves).map(function(c) {
+            let icon = '<i class="circle" style="background: '+registries[c[0]]+'"></i>';
+            return [icon+'&nbsp;'+c[0], c[1]]
+        }));
+        let legend = L.control.layers(null, Object.assign({}, {
+            "<img src='http://environment.data.gov.uk/spatialdata/sites-of-special-scientific-interest-england/wms?request=GetLegendGraphic&version=1.3.0&format=image/png&layer=Sites_of_Special_Scientific_Interest_England&style=default&'/>&nbsp;England SSSIs": england,
+            "<img src='http://cagmap.snh.gov.uk/arcgis/services/snh_protected_sites/MapServer/WMSServer?service=WMS&request=GetLegendGraphic&version=1.3.0&format=image/png&layer=2&'/>&nbsp;Scotland SSSIs": scotland,
+            "<img src='http://datamap.gov.wales/geoserver/inspire-nrw/ows?service=WMS&request=GetLegendGraphic&format=image/png&layer=NRW_SSSI&'/>&nbsp;Wales SSSIs": wales,
+        }, caves));
+        legend.addTo(map);
+    });
+
+// UI layers
+let infobox = L.control({position: 'bottomright'});
+infobox.onAdd = function(map) {
+    let div = L.DomUtil.create('div', 'infobox');
+    div.innerHTML += '<h4>SSSI Cave Entrances</h4>' +
+        'Map of cave features on SSSIs in Great Britain.<br><br>Zoom in for SSSI boundaries. Click on markers for details.' +
+        '<br><br><a href="">Sources and more info</a>.';
+    return div;
+};
+infobox.addTo(map);
+
+// Finishing touches (botch job)
+window.onload = function() {
+    document.getElementsByClassName('leaflet-control-layers-overlays')[0].insertAdjacentHTML('beforebegin', '<h4><b>SSSIs</b></h4>');
+    document.getElementsByClassName('leaflet-control-layers-overlays')[0].children[3].insertAdjacentHTML('beforebegin', '<p></p><h4><b>Caves</b></h4>');
+}
